1067a3f8b7d0e0e16d159b4702b4b500f77c588f
Joan Gimenez Donat <joan16v@gmail.com>
2018-01-17 12:21:16 +0100

    Update publicar_denuncia.php

--- publicar_denuncia.php
+++ publicar_denuncia.php
@@ -2,12 +2,6 @@
 
 include('include/application_top.php');
 
-mysql_query("update denuncias set descripcion='".strip_tags(addslashes($_POST['descripcion']))."', activa=1 where id='".$_POST['id']."'");
-
-$amigable=formatearNombre($_POST['descripcion']);
-
-//mail("joan16v@gmail.com","foto-denuncia publicada","foto-denuncia publicada");
-
-header("Location: foto.php?denuncia=".$_POST['id']."&desc=".$amigable);
-
-?>
+mysql_query("update denuncias set descripcion='" . strip_tags(addslashes($_POST['descripcion'])) . "', activa=1 where id='" . $_POST['id'] . "'");
+$amigable = formatearNombre($_POST['descripcion']);
+header("Location: foto.php?denuncia=" . $_POST['id'] . "&desc=" . $amigable);
