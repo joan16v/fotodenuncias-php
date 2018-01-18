595657d57b0dc7e8e7af32700e7c49268c63bb27
Joan Gimenez Donat <joan16v@gmail.com>
2018-01-18 08:40:51 +0100

    Update metas.php

--- metas.php
+++ metas.php
@@ -1,9 +1,8 @@
-<link rel="shortcut icon" href="icono.ico">	
-
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-<meta name="TITLE" content="foto denuncias, denuncialo con foto, denuncias, fotografias, fotodenuncia social.">
-<meta name="DESCRIPTION" content="foto denuncias, denuncialo con foto, denuncias, fotografias, fotodenuncia social.">
-<meta name="KEYWORDS" content="foto denuncias, denuncialo con foto, denuncias, fotografias, fotodenuncia social.">
-<meta name="OWNER" content="joan16v">
-<meta name="AUTHOR" content="joan16v">
-<meta name="RATING" content="joan16v">
+<link rel="shortcut icon" href="icono.ico" />
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta name="TITLE" content="foto denuncias, denuncialo con foto, denuncias, fotografias, fotodenuncia social." />
+<meta name="DESCRIPTION" content="foto denuncias, denuncialo con foto, denuncias, fotografias, fotodenuncia social." />
+<meta name="KEYWORDS" content="foto denuncias, denuncialo con foto, denuncias, fotografias, fotodenuncia social." />
+<meta name="OWNER" content="joan16v" />
+<meta name="AUTHOR" content="joan16v" />
+<meta name="RATING" content="joan16v" />
