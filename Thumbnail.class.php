4412de87b2f6c8191f369eae5cef188728450998
Joan Gimenez Donat <joan16v@gmail.com>
2018-02-23 08:03:59 +0100

    Update Thumbnail.class.php

--- Thumbnail.class.php
+++ Thumbnail.class.php
@@ -286,24 +286,21 @@ class Thumbnail
     *@access public
     *@return boolean
     **/
-    function process () {
+    function process ()
+    {
+        ini_set('memory_limit', $this->memory_limit);
+        ini_set('max_execution_time', $this->max_execution_time);
 
-        ini_set('memory_limit',$this->memory_limit);
-        ini_set('max_execution_time',$this->max_execution_time);
+        $X_des = $this->img["x_thumb"];
+        $Y_des = $this->img["y_thumb"];
 
-        $X_des =$this->img["x_thumb"];
-        $Y_des =$this->img["y_thumb"];
+        $gd_version = $this->gdVersion();
+        if ($gd_version >= 2) {
+                $this->img["des"] = ImageCreateTrueColor($X_des, $Y_des);
 
-        //if ($this->checkgd2()) {
-        $gd_version=$this->gdVersion();
-        if ($gd_version>=2) {
-        //if (false) {
-
-                $this->img["des"] = ImageCreateTrueColor($X_des,$Y_des);
-
-                if ($this->txt_watermark!='' ) {
+                if ($this->txt_watermark != '' ) {
                     sscanf($this->txt_watermark_color, "%2x%2x%2x", $red, $green, $blue);
-                    $txt_color=imageColorAllocate($this->img["des"] ,$red, $green, $blue);
+                    $txt_color = imageColorAllocate($this->img["des"], $red, $green, $blue);
                 }
 
                 if (!$this->bicubic_resample) {
