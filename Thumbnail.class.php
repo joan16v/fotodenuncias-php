a345807d91999fb839acf063f3dcd04d393a33b0
Joan Gimenez Donat <joan16v@gmail.com>
2018-06-01 07:58:30 +0200

    Update Thumbnail.class.php

--- Thumbnail.class.php
+++ Thumbnail.class.php
@@ -302,7 +302,7 @@ class Thumbnail
 
                 if ($this->txt_watermark != '') {
                     sscanf($this->txt_watermark_color, "%2x%2x%2x", $red, $green, $blue);
-                    $txt_color = imageColorAllocate($this->img["des"], $red, $green, $blue);
+                    $txt_color = imageColorAllocate($this->img['des'], $red, $green, $blue);
                 }
 
                 if (!$this->bicubic_resample) {
