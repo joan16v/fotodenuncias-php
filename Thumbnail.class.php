be24bd233957b38b9a9d0f5cf963acce72fb568f
Joan Gimenez Donat <joan16v@gmail.com>
2018-07-09 09:33:35 +0200

    Update Thumbnail.class.php

--- Thumbnail.class.php
+++ Thumbnail.class.php
@@ -276,7 +276,7 @@ class Thumbnail
         if ($this->output_format == 'PNG') {
             imagePNG($this->img['des'], "$save");
         } else {
-            imageinterlace( $this->img["des"], $this->jpeg_progressive);
+            imageinterlace( $this->img['des'], $this->jpeg_progressive);
             imageJPEG($this->img["des"], "$save", $this->quality);
         }
 
