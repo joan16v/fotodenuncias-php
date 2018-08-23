7e10f148492844eb5731a0ebbc9e6882b9e04e23
Joan Gimenez Donat <joan16v@gmail.com>
2018-08-23 08:43:48 +0200

    Update Thumbnail.class.php

--- Thumbnail.class.php
+++ Thumbnail.class.php
@@ -332,7 +332,7 @@ class Thumbnail
                     $this->img['watermark'] = imagecreatefrompng($this->img_watermark);
                     $this->img['x_watermark'] = imagesx($this->img["watermark"]);
                     $this->img["y_watermark"] = imagesy($this->img["watermark"]);
-                    imagecopy($this->img["des"], $this->img["watermark"], $this->calc_position_H (), $this->calc_position_V (), 0, 0, $this->img["x_watermark"], $this->img["y_watermark"]);
+                    imagecopy($this->img['des'], $this->img["watermark"], $this->calc_position_H (), $this->calc_position_V (), 0, 0, $this->img["x_watermark"], $this->img["y_watermark"]);
                 }
                 imagecopyresized($this->img['des'], $this->img['src'], 0, 0, 0, 0, $X_des, $Y_des, $this->img['x'], $this->img["y"]);
                 @imagecopy ($this->img['des'], $this->img['watermark'], $this->calc_position_H(), $this->calc_position_V(), 0, 0, $this->img["x_watermark"], $this->img["y_watermark"]);
