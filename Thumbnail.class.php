3daa560d06a8c724a504adc21475c9b6852dc0a7
Joan Gimenez Donat <joan16v@gmail.com>
2018-08-24 08:54:20 +0200

    Update Thumbnail.class.php

--- Thumbnail.class.php
+++ Thumbnail.class.php
@@ -652,7 +652,7 @@ class Thumbnail
                     imagesetpixel($s, $x, $y, $c);
                 }
             }
-            imagesavealpha($s,true);
+            imagesavealpha($s, true);
 
             return $s;
         }
