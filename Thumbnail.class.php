3c747893270bbb7d951b9e5bfbe84b89c559b39e
Joan Gimenez Donat <joan16v@gmail.com>
2018-12-17 08:06:38 +0100

    Update Thumbnail.class.php

--- Thumbnail.class.php
+++ Thumbnail.class.php
@@ -328,7 +328,7 @@ class Thumbnail
             if ($this->txt_watermark != '') {
                 sscanf($this->txt_watermark_color, "%2x%2x%2x", $red, $green, $blue);
                 $txt_color=imageColorAllocate($this->img["des"] ,$red, $green, $blue);
-                }
+            }
 
                 if ($this->img_watermark != '' && file_exists($this->img_watermark)) {
                     $this->img['watermark'] = imagecreatefrompng($this->img_watermark);
