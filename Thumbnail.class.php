675b2fdc8a80cbda23c9d5a4b7f46362fc22a68a
Joan Gimenez Donat <joan16v@gmail.com>
2019-06-12 08:55:54 +0200

    Update Thumbnail.class.php

--- Thumbnail.class.php
+++ Thumbnail.class.php
@@ -652,7 +652,7 @@ class Thumbnail
 
                 $t = ($t > 127) ? 127 : $t;
                 $t = 127 - $t;
-                    $c = imagecolorallocatealpha($s, $c['red'], $c['green'], $c['blue'], $t);
+                $c = imagecolorallocatealpha($s, $c['red'], $c['green'], $c['blue'], $t);
                     imagesetpixel($s, $x, $y, $c);
                 }
             }
